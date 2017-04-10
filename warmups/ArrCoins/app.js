395c4ca740b69d2c8f31398b6023cc6de13f3cfe
--global <--global>
2017-04-10 11:30:56 -0400

    first commit

---
+++ warmups/ArrCoins/app.js
@@ -0,0 +1,9 @@
+//write a function that takes an array of coins, and a string representing a coin. Options to the strings in the array, and the the string representing a coin are restricted to: penny, nickel, dime, quarter, half dollar, and dollar.
+
+//This function will output how many of the second argument (the string) it would take to cover the total value of all of the coins in the array.
+//
+//Tests:
+//
+//`coverValue(["penny", "nickel", "dime"], "quarter") // -> 1`
+//`coverValue(["penny", "nickel", "dime"], "nickel") // -> 4`
+//`coverValue(["penny", "nickel", "dime", "dime", "quarter"], "dime")`
